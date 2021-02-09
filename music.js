b0918b1737c0be64271c4b8591b3ca5df2b70c0b
Fahim <Shahriarfahim19@gmail.com>
2021-02-09 19:20:25 +0600

    Music App

---
+++ music.js
@@ -0,0 +1,74 @@
+const searchBTN = document.getElementById('search-btn');
+const songDisplay = document.getElementById('songDisplay');
+const songLyrics = document.getElementById('songLyrics');
+const errorTag = document.getElementById('error-msg');
+searchBTN.addEventListener('click', getSongName);
+
+async function getSongName() {
+    const searchSong = document.getElementById('search-song').value;
+    const searchURL = `https://api.lyrics.ovh/suggest/${searchSong}`;
+    try {
+        const response = await fetch(searchURL);
+        const data = await response.json();
+        displaySong(data.data) 
+    } catch (error) {
+        songDisplay.innerHTML='';
+        displayError("Sorry! This Song is Not Available at this moment")
+    }
+    //console.log(searchURL)
+    
+    // console.log(data);
+    // return data;
+}
+// getSongName.then(data=>{ // Can't do that because click er time e already function call hoye gese.
+//     displaySong(data);
+// });
+
+const displaySong = songs => {
+    let markup = '';
+    errorTag.innerHTML='';
+    songs.forEach(song => {
+        markup += `
+           <div class="search-result col-md-8 mx-auto py-4">
+            <div class="single-result row align-items-center my-3 p-3">
+              <div class="col-md-9">
+                <h3 class="lyrics-name">${song.title}</h3>
+                <p class="author lead">Album by <span>${song.artist.name}</span></p>
+                <audio controls>
+                <source src="${song.preview}" type="audio/mpeg">
+                </audio>
+              </div>
+             
+              <div class="col-md-3 text-md-right text-center">
+                <button onClick="getLyrics('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
+              </div>
+            </div>
+          </div>
+           `
+    });
+    songLyrics.innerHTML = '';
+    songDisplay.innerHTML = markup;
+}
+
+async function getLyrics(artist, title) {
+    const lyricsURL = `https://api.lyrics.ovh/v1/${artist}/${title}`
+    try {
+        const response = await fetch(lyricsURL)
+        const data = await response.json();
+        displayLyrics(data.lyrics);
+    } catch (error) {
+        displayError("Oppss!Lyrics Not Available");
+    }
+
+}
+
+const displayLyrics = lyrics => {
+    errorTag.innerHTML='';
+    songLyrics.innerText = lyrics;
+
+}
+
+const displayError = error => {
+   
+    errorTag.innerText = error;
+}
